18f1c573b68d240e8b533fba05fb1a4731c64a23
Saptarshi Datta <saptarshidatta08@gmail.com>
2025-06-10 21:49:07 +0530

    day 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,57 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import Home from "./pages/Home.jsx";
+import Login from "./pages/Login.jsx";
+import Protected from "./components/AuthLayout.jsx";
+import Signup from "./pages/Signup.jsx";
+import AllPosts from "./pages/AllPosts.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <Protected authentication={false}>
+            <Login />
+          </Protected>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <Protected authentication={false}>
+            <Signup />
+          </Protected>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <Protected authentication={true}>
+            <AllPosts />
+          </Protected>
+        ),
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </StrictMode>
+);
---
+++ src/pages/AllPosts.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const AllPosts = (props) => {
+  return (
+    <div>
+      
+    </div>
+  )
+};
+
+export default AllPosts;
